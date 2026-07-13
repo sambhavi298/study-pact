bc29988914e80337acdf3535e788fe7f5167db25
sambhavi298 <sambhaviranjan257@gmail.com>
2026-07-13 18:15:31 +0530

    feat: add Prisma ORM with User model and frst migration

---
+++ server/src/repositories/user.repository.ts
@@ -0,0 +1,9 @@
+import { prisma } from "../config/prisma";
+
+export function createUser(email: string, name: string) {
+  return prisma.user.create({ data: { email, name } });
+}
+
+export function findUserByEmail(email: string) {
+  return prisma.user.findUnique({ where: { email } });
+}
